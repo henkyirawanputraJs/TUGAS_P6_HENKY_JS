3b70adafdc44f95269a8c3d950348a9f7512559c
akmal123456 <afdalbeand11@gmail.com>
2023-11-22 20:37:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let Nama_Mahasiswa ='reni'
+if(Nama_Mahasiswa==='Salma'){
+    console.log("Mahasiswa yang bernama Salma mengambil kelas JavaScript, kelas tersebut dimulai pada jam 10:00 AM")
+}else if(Nama_Mahasiswa==='Andre'){
+    console.log('"Mahasiswa yang bernama Andre mengambil kelas Design Grafis, kelas tersebut dimulai pada jam 13:00 PM"')
+
+}else{
+    console.log('Mohon maaf anda belum mengambil kelas kafe koding')
+}
+console.log(Nama_Mahasiswa)
